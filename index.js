ac2394fbb0baccfb573285cadd3af988887c08d0
onezens <leafnet02@qq.com>
2018-01-28 16:16:42 +0800

    fix some issues and add fetch func

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 /**
  * Created by wz on 2017/11/22.
  */
-
+// process.env.DEBUG="DCL*";
 require('./lib/app');
+require('./lib/fetch/fetchBDVideo/fetchBDVideo');
+
+console.log(process.env.DEBUG);
